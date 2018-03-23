5844f6a06f19146862354656df2f92d0a0eb6ca7
gnayuy <yangy@ibs-yangy-ux1.corp.alleninstitute.org>
2018-03-23 15:57:13 -0700

    add swc sampling script

---
+++ fMOST/script/sampleswc.sh
@@ -0,0 +1,51 @@
+
+# sh sampleswc.sh input.swc output.swc scale
+
+# input
+ISWC=$1
+OSWC=$2
+
+SCALE=$3
+
+SR=`echo print 1.0 / $SCALE | python`
+
+#
+while read line
+do
+
+if [[ $line =~ "#" ]]
+then
+
+echo $line >> ${OSWC}
+
+else
+
+str=($line)
+
+N=${str[0]}
+TYPE=${str[1]}
+X=${str[2]}
+Y=${str[3]}
+Z=${str[4]}
+RADIUS=${str[5]}
+PN=${str[6]}
+
+X=`echo "$X * $SR" | bc`
+Y=`echo "$Y * $SR" | bc`
+Z=`echo "$Z * $SR" | bc`
+
+if [[ $PN -eq -1 ]]
+then
+
+echo "$((N-NUM)) $TYPE $X $Y $Z $RADIUS $PN" >> ${OSWC}
+
+else
+
+echo "$((N-NUM)) $TYPE $X $Y $Z $RADIUS $((PN-NUM))" >> ${OSWC}
+
+fi
+
+fi
+
+done < ${ISWC}
+
