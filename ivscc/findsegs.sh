55e3e4af9dadef67aecc758235a2e07ca06eb60b
gnayuy <yangy@ibs-yangy-ux1.corp.alleninstitute.org>
2018-05-10 13:01:31 -0700

    add script

---
+++ ivscc/findsegs.sh
@@ -0,0 +1,76 @@
+
+
+for i in *manualtraced*.swc;
+do
+
+n=${i%*_ds4*}
+
+#echo $n
+
+#
+while read line;
+do
+
+if [[ $line != *"#"* ]]
+then
+
+str=($line)
+
+if(( ${str[1]} == 3 ))
+then
+
+goodout="goodden.txt"
+badout="badden.txt"
+
+else
+
+goodout="goodaxon.txt"
+badout="badaxon.txt"
+
+fi
+
+fi
+
+done < $i
+
+m=${i%*.swc}
+m=${m#*manualtraced*}
+
+file=${i%*_resampled*}"_estimated_seg"$m".dist"
+
+if [ -f "$file" ]
+then
+
+while read line
+do
+
+if [[ $line =~ "bi-directional" ]]
+then
+
+score=${line#*=\ *}
+
+fi
+
+done < $file
+
+result=$(echo "$score < 2" | bc)
+
+if (( $result ))
+then
+
+echo $score $goodout
+
+echo $i >> $goodout
+
+else
+
+echo $score $badout
+
+echo $i >> $badout
+
+fi
+
+
+fi
+
+done
