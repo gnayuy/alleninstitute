1b264f163163a73b2cffe0d19954835d0c2b9f91
gnayuy <yangy@ibs-yangy-ux1.corp.alleninstitute.org>
2018-05-10 09:40:40 -0700

    add script

---
+++ ivscc/summaryscores.sh
@@ -0,0 +1,112 @@
+
+curnum=0
+
+for i in */*dist.txt;
+do
+
+num=${i%*/*}
+
+if [[ $curnum -eq $num ]]
+then
+
+n=$((n+1))
+
+else
+
+n=0
+curnum=$num
+
+fi
+
+if [[ $i =~ axon ]]
+then
+
+#
+while read line
+do
+
+if [[ $line =~ "bi-directional" ]]
+then
+
+ascore=${line#*=\ *}
+
+elif [[ $line =~ "differen-structure-average" ]]
+then
+
+adscore=${line#*=\ *}
+
+elif [[ $line =~ "percent of different-structure (average)" ]]
+then
+
+adpscore=${line#*=\ *}
+
+fi
+
+
+done < $i
+
+
+
+elif [[ $i =~ dendrite ]]
+then
+
+#
+while read line
+do
+
+if [[ $line =~ "bi-directional" ]]
+then
+
+dscore=${line#*=\ *}
+
+elif [[ $line =~ "differen-structure-average" ]]
+then
+
+ddscore=${line#*=\ *}
+
+elif [[ $line =~ "percent of different-structure (average)" ]]
+then
+
+ddpscore=${line#*=\ *}
+
+fi
+
+
+done < $i
+
+else
+
+#
+while read line
+do
+
+if [[ $line =~ "bi-directional" ]]
+then
+
+score=${line#*=\ *}
+
+elif [[ $line =~ "differen-structure-average" ]]
+then
+
+dscore=${line#*=\ *}
+
+elif [[ $line =~ "percent of different-structure (average)" ]]
+then
+
+dpscore=${line#*=\ *}
+
+fi
+
+done < $i
+
+
+fi
+
+if((n==2))
+then
+echo "$num, $score, $dscore, $dpscore, $ascore, $adscore, $adpscore, $dscore, $ddscore, $ddpscore"
+fi
+
+
+done
+
